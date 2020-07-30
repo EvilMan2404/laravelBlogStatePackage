065015213050330e9213e0f5a76fbc5b4e697c78
EvilMan2404 <39568121+EvilMan2404@users.noreply.github.com>
2020-07-30 23:57:41 +0300

    Update 2020_07_27_195151_create_posts_table.php

--- src/migrations/2020_07_27_195151_create_posts_table.php
+++ src/migrations/2020_07_27_195151_create_posts_table.php
@@ -16,6 +16,7 @@ class CreatePostsTable extends Migration
         if (!Schema::hasTable('posts')) {
             Schema::create('posts', function (Blueprint $table) {
                 $table->bigIncrements('id');
+                $table->string('name');
                 $table->timestamps();
             });
         }
